bd0f0d83aa9fc0a8ab2e6b51381d12d842709f7b
TomasDangelo <104146883+TomasDangelo@users.noreply.github.com>
2023-01-04 11:10:01 -0300

    Inicio proyecto

---
+++ main.js
@@ -0,0 +1,17 @@
+import {express} from 'express';
+import {Router} from 'express-router';
+
+const app = express();
+const router = new Router();
+
+
+
+
+router.get('/' , (req,res) => {
+    
+})
+
+
+app.use('/api' , (req,res) => {
+
+})
